9b90c24c3cad7370e07feffceaca1b365ec65a80
tranquocdong1 <tranquocdong10.2@gmail.com>
2024-11-05 20:27:52 +0700

    first

---
+++ src/components/ProductList.js
@@ -0,0 +1,31 @@
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+
+const ProductList = () => {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    axios.get('http://localhost:3000/api/products')
+      .then(response => {
+        setProducts(response.data);
+      })
+      .catch(error => {
+        console.error('Error fetching products:', error);
+      });
+  }, []);
+
+  return (
+    <div>
+      <h2>Product List</h2>
+      <ul>
+        {products.map(product => (
+          <li key={product._id}>
+            {product.name} - ${product.price}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default ProductList;
